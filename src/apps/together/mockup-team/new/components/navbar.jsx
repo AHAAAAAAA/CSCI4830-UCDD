fa3766902bf61bfed01e701e9e8a72f79e1d4e72
Sigrunn Sky <sigrunn.sky@colorado.edu>
2016-03-16 14:42:22 -0600

    :D

--- src/apps/together/mockup-team/new/components/navbar.jsx
+++ src/apps/together/mockup-team/new/components/navbar.jsx
@@ -4,12 +4,11 @@ class NavBar extends React.Component {
     if(this.props.data.user){
       return (
       <nav>
-        <div className="nav-wrapper black">
+        <div className="nav-wrapper light-green darken-3">
           <div className="container">
-            <a href="/apps/uber/index.html" className="brand-logo">Revolver</a>
+            <a href="/apps/uber/index.html" className="brand-logo">Polive</a>
             <ul id="nav-mobile" className="right hide-on-med-and-down">
-              <li><a href="/apps/uber/client/index.html"><i className="material-icons left">room</i> Provider Map</a></li>
-              <li><a href="/apps/uber/admin/index.html"><i className="material-icons left">supervisor_account</i> Admin</a></li>
+              <li><a href="/apps/uber/client/index.html">Home</a></li>
               <li><a className="white-text" href="#"><i className="material-icons left">perm_identity</i> {this.props.data.user.username}</a></li>
               <li><a href="#" onClick={this.props.actions.logout}> Logout</a></li>
             </ul>
@@ -21,11 +20,11 @@ class NavBar extends React.Component {
     else{
       return (
         <nav>
-          <div className="nav-wrapper black">
+          <div className="nav-wrapper light-green darken-3">
             <div className="container">
-              <a href="/apps/uber/index.html" className="brand-logo">Revolver</a>
+              <a href="/apps/uber/index.html" className="brand-logo">Polive</a>
               <ul id="nav-mobile" className="right hide-on-med-and-down">
-                <li><a href="/apps/uber/client/index.html"><i className="material-icons left">room</i> Provider Map</a></li>
+                <li><a href="/apps/uber/client/index.html">Home</a></li>
                 <li><a href="#" onClick={this.props.actions.login}> Login</a></li>
               </ul>
             </div>
