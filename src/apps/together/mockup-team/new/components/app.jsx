257fbe7801f69f3c24e10ccb6eab47eae15cfcf6
jcherrera <j.c.h@me.com>
2016-03-16 18:48:44 -0600

    working login

--- src/apps/together/mockup-team/new/components/app.jsx
+++ src/apps/together/mockup-team/new/components/app.jsx
@@ -2,8 +2,7 @@ class App extends React.Component {
   render(){
     
     return (
-      <div className="container">
-        <h3><i className="some-user"></i> ASD </h3>
+      <div className="container center">
      </div>
       );
   } 
