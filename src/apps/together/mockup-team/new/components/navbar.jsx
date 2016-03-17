842dc5b5540fabc336972b169675cca6bf5c9976
Sigrunn Sky <sigrunn.sky@colorado.edu>
2016-03-17 07:51:24 -0600

    Small Tweaks

--- src/apps/together/mockup-team/new/components/navbar.jsx
+++ src/apps/together/mockup-team/new/components/navbar.jsx
@@ -10,7 +10,7 @@ class NavBar extends React.Component {
       constrain_width: false, // Does not change width of dropdown to that of the activator
       hover: true, // Activate on hover
       gutter: 0, // Spacing from edge
-      belowOrigin: false, // Displays dropdown below the button
+      belowOrigin: true, // Displays dropdown below the button
       alignment: 'left' // Displays dropdown with edge aligned to the left of button
     }
   );
@@ -22,12 +22,16 @@ class NavBar extends React.Component {
       <nav>
         <div className="nav-wrapper light-green darken-3">
           <div className="container">
-            <a href="/apps/together/mockup-team/index.html" className="brand-logo">Polive <img src="/apps/together/mockup-team/categories/olive.png" height="20px" style="margin-top:15px;"/> </a>
+            <a href="/apps/together/mockup-team/new/index.html" className="brand-logo">Polive <img src="/apps/together/mockup-team/categories/olive.png" height="20px" style="margin-top:15px;"/> </a>
             <ul id="nav-mobile" className="right hide-on-med-and-down">
               <li><a href="/apps/together/mockup-team/index.html">Home</a></li>
               <li><a className="white-text" href="#"><i className="material-icons left">perm_identity</i> {this.props.data.user.username}</a></li>
-
               <li><a className="dropdown-button white-text" href="#!" data-activates="categories_dropdown">Categories<i className="material-icons right">arrow_drop_down</i></a>
+                  <ul id='categories_dropdown' className='dropdown-content'>
+                  <li><a href="/apps/together/mockup-team/new/categories/Room1.html">Sports</a></li>
+                  <li><a href="/apps/together/mockup-team/new/categories/Room2.html">Politics</a></li>
+                  <li><a href="/apps/together/mockup-team/new/categories/Room3.html">Other</a></li>
+                  </ul>
               </li>
               <li><a href="#" onClick={this.props.actions.logout}> Logout</a></li>
             </ul>
@@ -41,16 +45,16 @@ class NavBar extends React.Component {
         <nav>
           <div className="nav-wrapper light-green darken-3">
             <div className="container">
-              <a href="/apps/uber/index.html" className="brand-logo">Polive</a>
+              <a href="/apps/together/mockup-team/new/index.html" className="brand-logo">Polive</a>
               <a href="">{this.props.data.category}</a>
               <ul id="nav-mobile" className="right hide-on-med-and-down">
                 <li><a href="/apps/together/mockup-team/new/index.html">Home</a></li>
                 <li><a href="#" onClick={this.props.actions.login}> Login</a></li>
                 <li><a className="dropdown-button white-text" href="#!" data-activates="categories_dropdown">Categories<i className="material-icons right">arrow_drop_down</i></a>
                   <ul id='categories_dropdown' className='dropdown-content'>
-                  <li><a href="#!" onClick={this.props.actions.selectCategoryOne}>one</a></li>
-                  <li><a href="#!">two</a></li>
-                  <li><a href="#!">three</a></li>
+                  <li><a href="/apps/together/mockup-team/new/categories/Room1.html">Sports</a></li>
+                  <li><a href="/apps/together/mockup-team/new/categories/Room2.html">Politics</a></li>
+                  <li><a href="/apps/together/mockup-team/new/categories/Room3.html">Other</a></li>
                   </ul>
 
                 </li>
