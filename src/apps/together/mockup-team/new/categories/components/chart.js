fe813129543aae00e4bc7c6b3d364eeea4ad39c1
Sigrunn Sky <sigrunn.sky@colorado.edu>
2016-03-20 14:23:26 -0600

    I wonder if there is something basic that I am missing. I can't seem to get react working.

--- src/apps/together/mockup-team/new/categories/components/chart.js
+++ src/apps/together/mockup-team/new/categories/components/chart.js
@@ -1,19 +1,19 @@
 MyComponents.votes = React.createClass({
   render: function() {
-    
-  log("snorre king")
-
     return (
-      <div className="right col l5 m5 s5" style="margin-top:14%;">
-        <div className="card-panel light-green darken-1">
-          <h4 className="card-title" className="center"><b>Live Poll</b></h4>
-        <canvas id="myChart" className="center"></canvas>
-        <div className="row center">
-          <a className="center waves-effect waves-light red btn">Red</a>
-          <a className="center waves-effect waves-light blue btn">Blue</a>
-       </div>
-      </div>
-     </div>
+      <span> {this.props.votes.no} + "  ss" + {this.props.votes.yes}
+     </span>
+     //  <div className="right col l5 m5 s5" style="margin-top:14%;">
+     //    <div className="card-panel light-green darken-1">
+     //      <h4 className="card-title" className="center"><b>Live Poll</b></h4>
+     //    <canvas id="myChart" className="center"></canvas>
+     //    <div className="row center">
+     //      <a className="center waves-effect waves-light red btn">Red</a>
+     //      <a className="center waves-effect waves-light blue btn">Blue</a>
+     //   </div>
+     //  </div>
+     // </div>
     );
   }
 });
+
