861b2187b1d5cd18822ec71c76e4203ef98c42ec
Sigrunn Sky <sigrunn.sky@colorado.edu>
2016-03-21 13:15:56 -0600

    making the reactjs navbar look more like the original .. added the olives icon and whatnot.

--- src/apps/together/mockup-team/new/components/navbar.jsx
+++ src/apps/together/mockup-team/new/components/navbar.jsx
@@ -70,7 +70,7 @@ class NavBar extends React.Component {
       <nav>
         <div className="nav-wrapper light-green darken-3">
           <div className="container">
-            <a href="/apps/together/mockup-team/new/index.html" className="brand-logo">Polive</a>
+          	<a href="/apps/together/mockup-team/new/index.html" className="brand-logo center">Polive<img src="/apps/together/mockup-team/new/images/olive.png" height="20px" style={{marginTop: 15}} /></a>
 			<a href="#">{this.props.data.category}</a>
             <ul id="nav-mobile" className="right hide-on-med-and-down">
 			  <li><a href="/apps/together/mockup-team/new/index.html">Home</a></li>
@@ -90,13 +90,13 @@ class NavBar extends React.Component {
         <nav>
           <div className="nav-wrapper light-green darken-3">
             <div className="container">
-              <a href="/apps/together/mockup-team/new/index.html" className="brand-logo">Polive</a>
+            <a href="/apps/together/mockup-team/new/index.html" className="brand-logo center">Polive2<img src="/apps/together/mockup-team/new/images/olive.png" height="20px" style={{marginTop: 15}} /></a>
 			  <a href="#">{this.props.data.category}</a>
               <ul id="nav-mobile" className="right hide-on-med-and-down">
 			    <li><a href="/apps/together/mockup-team/new/index.html">Home</a></li>
                 <li><a href="#" onClick={this.props.actions.login}> Login</a></li>
 				<li><a className="dropdown-button white-text" href="#!" data-activates="categories_dropdown">Categories<i className="material-icons right">arrow_drop_down</i></a>
-                  <ul id='categories_dropdown' className='dropdown-content'></ul>
+                <ul id='categories_dropdown' className='dropdown-content'></ul>
                 </li>
               </ul>
             </div>
