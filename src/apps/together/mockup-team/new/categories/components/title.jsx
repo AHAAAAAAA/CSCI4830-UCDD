b859c9d01dd227d3cc40a9fab83ae252d5f723fb
BenedictRM <mehringr@gmail.com>
2016-04-03 18:10:18 -0600

    order matters
    loading of scripts must be specifically loaded

--- src/apps/together/mockup-team/new/categories/components/title.jsx
+++ src/apps/together/mockup-team/new/categories/components/title.jsx
@@ -1,5 +1,6 @@
 class Title extends React.Component { 
   render(){
+    console.log("Currently in category: ", this.props.data.category);
     $('#categoryTitle').append('<h1 class="black-text center">'+this.props.data.category+'</h1>');
     return (
         null
--- src/apps/together/mockup-team/new/components/lobby.jsx
+++ src/apps/together/mockup-team/new/components/lobby.jsx
@@ -1,7 +1,7 @@
 class Room extends React.Component { 
   render(){
     this.props.data.room = 'Lobby'
-
+    console.log('user currently in the lobby!')
     return (
 	    null
     );
