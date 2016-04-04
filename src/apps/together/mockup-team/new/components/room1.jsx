36d1015b922bb8d894d6ad260838303bba6020b4
BenedictRM <mehringr@gmail.com>
2016-04-03 18:31:00 -0600

    current stuff
    here

--- src/apps/together/mockup-team/new/components/room1.jsx
+++ src/apps/together/mockup-team/new/components/room1.jsx
@@ -7,6 +7,12 @@ class Room extends React.Component {
     this.props.data.room = 'Room1'
 	console.log('Room1 set!')
 	
+	//If user logged in, make call to firebase to update their room
+	if(this.props.data.user){
+	    console.log('Changing rooms!')
+		//TODO: call this.props.actions.setUserRoom() once persistent logins works
+	}
+	
 	//Set data.category to current room category
 	roomtitle.once('value', function(data) {
 	var roomData = data.val();
